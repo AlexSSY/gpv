fec88f478dce66230eb3ef6d06386ba7d989ad00
AlexSSY <adolf5341@gmail.com>
2025-12-13 22:08:36 +0200

    add ruff

--- main.py
+++ main.py
@@ -14,25 +14,25 @@ class Shortage:
 
 
 MONTHS: dict[str, int] = {
-  "січня": 1,
-  "лютого": 2,
-  "березня": 3,
-  "квітня": 4,
-  "травня": 5,
-  "червня": 6,
-  "липня": 7,
-  "серпня": 8,
-  "вересня": 9,
-  "жовтня": 10,
-  "листопада": 11,
-  "грудня": 12
+    "січня": 1,
+    "лютого": 2,
+    "березня": 3,
+    "квітня": 4,
+    "травня": 5,
+    "червня": 6,
+    "липня": 7,
+    "серпня": 8,
+    "вересня": 9,
+    "жовтня": 10,
+    "листопада": 11,
+    "грудня": 12,
 }
 
 
 def parse_ua_date(string: str) -> time:
-  day, month_name, year, _ = string.split(' ', 3)
-  month = MONTHS[month_name]
-  return datetime(int(year), month, int(day))
+    day, month_name, year, _ = string.split(" ", 3)
+    month = MONTHS[month_name]
+    return datetime(int(year), month, int(day))
 
 
 def load_shortages() -> list[Shortage]:
@@ -40,19 +40,21 @@ def load_shortages() -> list[Shortage]:
 
     try:
         response = requests.get(url)
-        response.raise_for_status() 
+        response.raise_for_status()
         html_content = response.text
     except requests.exceptions.RequestException as e:
         print("OFF|Ошибка сети")
         exit()
 
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
 
     date_numbers = {}
-    gpv_divs = soup.find_all(class_='gpvinfodetail')
+    gpv_divs = soup.find_all(class_="gpvinfodetail")
     for gpv_div in gpv_divs:
-        date_string = gpv_div.find('b').text
-        tr = gpv_div.select_one('.turnoff-scheduleui-table > tbody:nth-child(2) > tr:nth-child(2)')
+        date_string = gpv_div.find("b").text
+        tr = gpv_div.select_one(
+            ".turnoff-scheduleui-table > tbody:nth-child(2) > tr:nth-child(2)"
+        )
         date = parse_ua_date(date_string)
         numbers = [
             int(td["class"][0].split("light_")[1])
@@ -72,26 +74,20 @@ def load_shortages() -> list[Shortage]:
             if n == 3 and start:
                 soft_finish = date + timedelta(seconds=idx * 30 * 60)
                 hard_finish = date + timedelta(seconds=(idx + 1) * 30 * 60)
-                shortages.append(
-                    Shortage(start, hard_finish, soft_finish is not None)
-                )
+                shortages.append(Shortage(start, hard_finish, soft_finish is not None))
                 start = None
                 soft_finish = None
                 continue
             if n == 1 and start:
                 hard_finish = date + timedelta(seconds=(idx + 1) * 30 * 60)
-                shortages.append(
-                    Shortage(start, hard_finish, False)
-                )
+                shortages.append(Shortage(start, hard_finish, False))
                 start = None
                 soft_finish = None
-    
+
     if start:
         date = list(date_numbers.keys())[-1]
         hard_finish = date + timedelta(seconds=48 * 30 * 60)
-        shortages.append(
-            Shortage(start, hard_finish, soft_finish is not None)
-        )
+        shortages.append(Shortage(start, hard_finish, soft_finish is not None))
 
     return shortages
 
@@ -103,7 +99,9 @@ if __name__ == "__main__":
     now = datetime.now()
 
     shortages = load_shortages()
-    current_shortage = next((m for m in shortages if now < m.end and now > m.start ), None)
+    current_shortage = next(
+        (m for m in shortages if now < m.end and now > m.start), None
+    )
 
     shortages_with_delta: dict[int, Shortage] = {}
 
@@ -118,9 +116,11 @@ if __name__ == "__main__":
         closest_shortage = shortages_with_delta[min_distance]
 
     if current_shortage is None:
-        print(f"OK|Свет есть до {closest_shortage.start.strftime("%H:%M")}")
+        print(f"OK|Свет есть до {closest_shortage.start.strftime('%H:%M')}")
     else:
         if current_shortage.has_yellow:
-            print(f"OFF|Света нет, может появиться в {(current_shortage.end - timedelta(minutes=30)).strftime("%H:%M")}")
+            print(
+                f"OFF|Света нет, может появиться в {(current_shortage.end - timedelta(minutes=30)).strftime('%H:%M')}"
+            )
         else:
-            print(f"UNCERTAIN|Точно будет в {current_shortage.end.strftime("%H:%M")}")
+            print(f"UNCERTAIN|Точно будет в {current_shortage.end.strftime('%H:%M')}")
